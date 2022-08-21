0dba5fa64a7d304419f32b3f6082bd16a6bf8467
Prometheus18082106 <u21451452@tuks.co.za>
2022-08-21 10:01:57 +0200

    EventHandler.js
    Created EventHandler.js and created EventHandler object inside with 4 empty member functions.

---
+++ EventHandler.js
@@ -0,0 +1,20 @@
+function EventHandler(events)
+{
+    this.events = events;
+
+    this.getEventsBetweenDates = function(start, end){
+
+    }
+
+    this.getByMonth = function(month){
+
+    }
+
+    this.getUniqueDateAndSort = function(){
+
+    }
+
+    this.getSummary = function(){
+        
+    }
+}
